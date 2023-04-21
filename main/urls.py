5dc007a2534f3b76351f793e794fc71879ace8a5
Леонтьев Игорь Иванович <leontev.ii@corp.bianca-city.ru>
2023-04-21 17:42:22 +0300

    learn added html o

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from . import views
+
+
+from django.urls import path
+
+urlpatterns = [   
+     path('',views.hello),
+     path('about', views.about)
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def hello(request):
+    return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about.html')
